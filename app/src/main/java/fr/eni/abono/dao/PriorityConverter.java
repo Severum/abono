a414d0f58cd1f32847f8ecc1a759e642e782f5ee
Romain Douteau <personnel@romaindouteau.fr>
2020-04-14 17:19:41 +0200

    Refactor code

--- app/src/main/java/fr/eni/abono/dao/PriorityConverter.java
+++ app/src/main/java/fr/eni/abono/dao/PriorityConverter.java
@@ -3,17 +3,16 @@ package fr.eni.abono.dao;
 import androidx.room.TypeConverter;
 
 import fr.eni.abono.bo.Priority;
-import fr.eni.abono.bo.Subscription;
 
 public class PriorityConverter {
 
     @TypeConverter
     public static Priority toPriority(String label) {
-        if (label == Priority.INDISPENSABLE.toString()) {
+        if (label.equals(Priority.INDISPENSABLE.toString())) {
             return Priority.INDISPENSABLE;
-        } else if (label == Priority.IMPORTANT.toString()) {
+        } else if (label.equals(Priority.IMPORTANT.toString())) {
             return Priority.IMPORTANT;
-        } else if (label == Priority.OPTIONAL.toString()) {
+        } else if (label.equals(Priority.OPTIONAL.toString())) {
             return Priority.OPTIONAL;
         } else {
             throw new IllegalArgumentException("Could not recognize priority");
