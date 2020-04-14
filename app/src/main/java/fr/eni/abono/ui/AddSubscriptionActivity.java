7149f98caf2c668ee183037c29ca25db4c1c42bb
Severum <mael.severe@laposte.net>
2020-04-14 16:45:15 +0200

    add subscription wip

---
+++ app/src/main/java/fr/eni/abono/ui/AddSubscriptionActivity.java
@@ -0,0 +1,14 @@
+package fr.eni.abono.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddSubscriptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_subscription);
+    }
+}
