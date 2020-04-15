225feb0ee79bad58f8f70e367dc64d9f26c47d19
Severum <mael.severe@laposte.net>
2020-04-15 11:49:53 +0200

    desfge

--- app/src/main/java/fr/eni/abono/ui/AddSubscriptionActivity.java
+++ app/src/main/java/fr/eni/abono/ui/AddSubscriptionActivity.java
@@ -21,7 +21,7 @@ public class AddSubscriptionActivity extends AppCompatActivity {
     private EditText editTextName;
     private EditText editTextDescription;
     private EditText editTextPrice;
-    private EditText editTextFrequency;
+    private Spinner frequencyDropDown;
     private Spinner dropdownPriority;
 
     @Override
@@ -32,17 +32,23 @@ public class AddSubscriptionActivity extends AppCompatActivity {
         editTextName = findViewById(R.id.editTextName);
         editTextDescription = findViewById(R.id.editTextDescription);
         editTextPrice = findViewById(R.id.editTextPrice);
-        editTextFrequency = findViewById(R.id.editTextFrequency);
+        frequencyDropDown = findViewById(R.id.frequencyDropDown);
+        String[] frequencyItems = new String[]{
+                "weekly",
+                "monthly",
+                "annually"
+        };
+        ArrayAdapter<String> frequencyAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, frequencyItems);
+        frequencyDropDown.setAdapter(frequencyAdapter);
 
         dropdownPriority = findViewById(R.id.priorityDropDown);
-        String[] items = new String[]{
+        String[] priorityItems = new String[]{
                 Priority.INDISPENSABLE.toString(),
                 Priority.IMPORTANT.toString(),
                 Priority.OPTIONAL.toString()
         };
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
-        dropdownPriority.setAdapter(adapter);
-
+        ArrayAdapter<String> priorityAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, priorityItems);
+        dropdownPriority.setAdapter(priorityAdapter);
     }
 
     public void validSubscription(View view) {
@@ -51,10 +57,39 @@ public class AddSubscriptionActivity extends AppCompatActivity {
             @Override
             public void run() {
                 float price = Float.parseFloat(editTextPrice.getText().toString());
-                float frequency = Float.parseFloat(editTextFrequency.getText().toString());
+                // float frequency = Float.parseFloat(editTextFrequency.getText().toString());
                 String name = String.valueOf(editTextName.getText());
                 String description = String.valueOf(editTextDescription.getText());
-                Subscription subscription = new Subscription(price, frequency, name, description, Priority.IMPORTANT);
+                float frequency = 1; // default annually
+                switch (frequencyDropDown.getSelectedItem().toString()) {
+                    case "weekly":
+                        frequency = (float)1/(float)53;
+                        break;
+                    case "monthly":
+                        frequency = (float)1/(float)12;
+                        break;
+                    case "annually":
+                        frequency = (float)1;
+                        break;
+                    default:
+                        break;
+                }
+                Subscription subscription;
+                switch (dropdownPriority.getSelectedItem().toString()) {
+                    case "Indispensable":
+                        subscription = new Subscription(price, frequency, name, description, Priority.INDISPENSABLE);
+                        break;
+                    case "Important":
+                        subscription = new Subscription(price, frequency, name, description, Priority.IMPORTANT);
+                        break;
+                    case "Optional":
+                        subscription = new Subscription(price, frequency, name, description, Priority.OPTIONAL);
+                        break;
+                    default:
+                        // TODO handle error
+                        subscription = null;
+                        break;
+                }
                 AppDatabase db = Connexion.getConnexion(AddSubscriptionActivity.this);
                 db.subscriptionDao().insert(subscription);
             }
