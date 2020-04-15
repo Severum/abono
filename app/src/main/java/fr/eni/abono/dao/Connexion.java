fea08977de2483842b358dfd79590d3c8e0606d0
Romain Douteau <personnel@romaindouteau.fr>
2020-04-15 11:09:57 +0200

    Add subscription feature

--- app/src/main/java/fr/eni/abono/dao/Connexion.java
+++ app/src/main/java/fr/eni/abono/dao/Connexion.java
@@ -1,12 +1,11 @@
 package fr.eni.abono.dao;
 
 import android.content.Context;
-import android.provider.DocumentsContract;
 
 import androidx.room.Room;
 
 public class Connexion {
     public static AppDatabase getConnexion(Context context) {
-        return Room.databaseBuilder(context, AppDatabase.class, "AbonoDB").build();
+        return Room.databaseBuilder(context, AppDatabase.class, "abono.db").build();
     }
 }
--- app/src/main/java/fr/eni/abono/ui/AddSubscriptionActivity.java
+++ app/src/main/java/fr/eni/abono/ui/AddSubscriptionActivity.java
@@ -2,33 +2,67 @@ package fr.eni.abono.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import fr.eni.abono.R;
 import fr.eni.abono.bo.Priority;
+import fr.eni.abono.bo.Subscription;
+import fr.eni.abono.dao.AppDatabase;
+import fr.eni.abono.dao.Connexion;
 
 public class AddSubscriptionActivity extends AppCompatActivity {
+
+    private EditText editTextName;
+    private EditText editTextDescription;
+    private EditText editTextPrice;
+    private EditText editTextFrequency;
+    private Spinner dropdownPriority;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_subscription);
 
-        //get the spinner from the xml.
-        Spinner dropdown = findViewById(R.id.priorityDropDown);
-        //create a list of items for the spinner.
-        String[] items = new String[]{Priority.INDISPENSABLE.toString(), Priority.IMPORTANT.toString(), Priority.OPTIONAL.toString()};
-        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
-        //There are multiple variations of this, but this is the basic variant.
+        editTextName = findViewById(R.id.editTextName);
+        editTextDescription = findViewById(R.id.editTextDescription);
+        editTextPrice = findViewById(R.id.editTextPrice);
+        editTextFrequency = findViewById(R.id.editTextFrequency);
+
+        dropdownPriority = findViewById(R.id.priorityDropDown);
+        String[] items = new String[]{
+                Priority.INDISPENSABLE.toString(),
+                Priority.IMPORTANT.toString(),
+                Priority.OPTIONAL.toString()
+        };
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
-        //set the spinners adapter to the previously created one.
-        dropdown.setAdapter(adapter);
+        dropdownPriority.setAdapter(adapter);
+
     }
 
     public void validSubscription(View view) {
 
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                float price = Float.parseFloat(editTextPrice.getText().toString());
+                float frequency = Float.parseFloat(editTextFrequency.getText().toString());
+                String name = String.valueOf(editTextName.getText());
+                String description = String.valueOf(editTextDescription.getText());
+                Subscription subscription = new Subscription(price, frequency, name, description, Priority.IMPORTANT);
+                AppDatabase db = Connexion.getConnexion(AddSubscriptionActivity.this);
+                db.subscriptionDao().insert(subscription);
+            }
+        }).start();
+
+        Log.d("validSubscription", "Subscription added in database");
+
+        Intent intentAddSubscription = new Intent(AddSubscriptionActivity.this, MainActivity.class);
+        startActivity(intentAddSubscription);
     }
 }
